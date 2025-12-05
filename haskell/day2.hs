2835c2366ff969d224df4dbe5819dedc35392bdf
Darcy Robson <17976794+drobson03@users.noreply.github.com>
2025-12-05 19:27:41 +1000

    day 2 part 2

--- haskell/day2.hs
+++ haskell/day2.hs
@@ -8,17 +8,25 @@ split delim str = takeWhile (/= delim) str : (split delim $ dropWhile (== delim)
 createRange :: [Int] -> [Int]
 createRange [x, y] = [x..y]
 
-isRepeating :: String -> Bool
-isRepeating str = let
+isRepeatingPart1 :: String -> Bool
+isRepeatingPart1 str = let
   halfLength = length str `div` 2
   in take halfLength str == drop halfLength str
+
+isRepeatingPart2 :: Int -> String -> Bool
+isRepeatingPart2 0 _ = False
+isRepeatingPart2 x str = let
+  parts = length str `div` x
+  sub = take x str
+  in (concat (take parts (repeat sub)) == str) || (isRepeatingPart2 (x - 1) str)
 
 main :: IO ()
 main = do
   input <- readFile "inputs/day2.txt"
 
-  let productIdRanges = takeWhile (/= '\n') input
+  let productIds = map show $ concatMap (createRange . (map parseInt) . (split '-')) $ split ',' $ takeWhile (/= '\n') input
 
-  print $ sum $ map parseInt $ filter isRepeating $ map show $ concatMap (createRange . (map parseInt) . (split '-')) $ split ',' $ productIdRanges
+  print $ sum $ map parseInt $ filter isRepeatingPart1 productIds
+  print $ sum $ map parseInt $ filter (\x -> isRepeatingPart2 (length x `div` 2) x) productIds
 
   return ()
